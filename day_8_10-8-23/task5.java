007b70069feb70bc3f42611fe8c00704253faafe
Himanshu <himanshuvirani23@gmail.com>
2023-08-11 10:44:00 +0530

    done

--- day_8_10-8-23/task5.java
+++ day_8_10-8-23/task5.java
@@ -5,32 +5,30 @@ public class task5 {
         System.out.println("Enter decemal num of your desire:");
         Scanner sc=new Scanner(System.in);
         double no=sc.nextDouble();
-       String s=String.valueOf(no);
-       int d=s.indexOf('.');
-        s.replace(".", "");
-        char[] c=s.toCharArray();
-        char[] r=new char[c.length+1];
-        int co=0;
-        for(int j=c.length-1;j>=0;j--)
-        { 
-            if (co < d)
-           {
-            r[co]=c[j];
-           }
-           else if (co == d)
-           {
-            r[co]='.';
-           }
-           else{
-           r[co] = c[j];}
-           co++;
-        }
-        
-        for(int l=0;l<r.length;l++)
+        String s=String.valueOf(no);
+        int len=s.length();
+        int ind=len-s.indexOf('.')-1;
+        int d=10;
+        int no2;
+        for(int l=1;l<ind;l++)
         {
-            System.out.println(r[l]);
+            d=d*10;
         }
-                    System.out.println(d);
+        no2= (int)(no*(d));
+        int newno=0;
+        while(no2>0)
+        {
+            newno=(newno*10)+no2%10;
+            no2/=10;
+        }
+        double no1=(double)(newno);
+        double no3=no1/d;
+                System.out.println(len+"=len "+ind+"=ind "+no2+"=no "+(d)+"=10^ind "+newno+"=newno "+no3+"=revers_no ");
 
+
+
+    
+    
     }
+      
 }
